c4a4f98dc56ba1cc224832cacfc4203ba962b8a4
zlolik <lolikandr@gmail.com>
2018-08-10 22:21:30 +0300

    move checking to WifiRouterChecker

--- app/application.cpp
+++ app/application.cpp
@@ -8,86 +8,102 @@ Timer tmr;
 // LED = D4 = GPIO2
 #define LED_PIN  2
 
-// Will be called when WiFi station network scan was completed
-void listNetworks(bool succeeded, BssList list) {
-	if (!succeeded) {
-		Serial.println("Failed to scan networks");
-		return;
-	}
-	for (int i = 0; i < list.count(); i++) {
-		Serial.print("\tWiFi: ");
-		Serial.print(list[i].ssid);
-		Serial.print(", ");
-		Serial.print(list[i].getAuthorizationMethodName());
-		if (list[i].hidden) Serial.print(" (hidden)");
-		Serial.println();
-	}
-}
+class WifiRouterChecker {
+public:
+  WifiRouterChecker(): failure_detected(false) {
+    // Set system ready callback method
+    System.onReady(Delegate<void()>(&WifiRouterChecker::onReady, this));
 
-void connectOk();
-void connectFail();
+    // Station - WiFi client
+    WifiStation.enable(true);
+    WifiStation.config(WIFI_SSID, WIFI_PWD); // Put you SSID and Password here
 
-void Reconnect() {
-  tmr.stop();
-  //
-  debugf("%9d: Reconnect", RTC.getRtcSeconds());
-  WifiStation.disconnect();
-  WifiStation.connect();
-  WifiStation.waitConnection(connectOk, 5, connectFail);
-  //
-  digitalWrite(RELAY_PIN, true);
-}
+    // Optional: Change IP addresses (and disable DHCP)
+    WifiStation.setIP(WIFI_STATIC_IP);
 
-// Will be called when WiFi station was connected to AP
-void connectOk()
-{
-	debugf("%9d: Connect successfull!", RTC.getRtcSeconds());
-	Serial.println(WifiStation.getIP().toString());
-  tmr.initializeMs(5000, TimerDelegate(&Reconnect)).start();
-  digitalWrite(RELAY_PIN, false);
-}
+  }
+  // Will be called when WiFi hardware and software initialization was finished
+  // And system initialization was completed
+  void onReady() {
+    debugf("%9d: WiFi ready, connect", RTC.getRtcSeconds());
+    connect(5);
+  }
+  // Will be called when WiFi station network scan was completed
+  void listNetworks(bool succeeded, BssList list) {
+    if (!succeeded) {
+      Serial.println("Failed to scan networks");
+      return;
+    }
+    for (int i = 0; i < list.count(); i++) {
+      Serial.print("\tWiFi: ");
+      Serial.print(list[i].ssid);
+      Serial.print(", ");
+      Serial.print(list[i].getAuthorizationMethodName());
+      if (list[i].hidden) Serial.print(" (hidden)");
+      Serial.println();
+    }
+  }
+private:
+  void reconnectAfterMs(uint32_t ms) {
+    tmr.initializeMs(ms, TimerDelegate(&WifiRouterChecker::reconnect, this)).start();
+  }
+  void reconnect() {
+    tmr.stop();
+    //
+    debugf("%9d: Reconnect", RTC.getRtcSeconds());
+    WifiStation.disconnect();
+    WifiStation.connect();
+    connect(5);
+    //
+    digitalWrite(RELAY_PIN, true);
+  }
 
-// Will be called when WiFi station timeout was reached
-void connectFail()
-{
-	debugf("%9d: Connect failed!", RTC.getRtcSeconds());
-  tmr.initializeMs(2000, TimerDelegate(&Reconnect)).start();
-  digitalWrite(RELAY_PIN, false);
-}
+  void connect(uint32_t timeoutSec) {
+    // Print available access points
+    WifiStation.startScan(Delegate<void(bool succeeded, BssList list)>(&WifiRouterChecker::listNetworks, this));
+    //
+    WifiStation.waitConnection(Delegate<void()>(&WifiRouterChecker::onConnectOk, this), timeoutSec, Delegate<void()>(&WifiRouterChecker::onConnectFail, this));
+  }
+  
+  // Will be called when WiFi station was connected to AP
+  void onConnectOk()
+  {
+    debugf("%9d: Connect successfull!", RTC.getRtcSeconds());
+    Serial.println(WifiStation.getIP().toString());
+    //
+    reconnectAfterMs(5000);
+    digitalWrite(RELAY_PIN, false);
+  }
 
-// Will be called when WiFi hardware and software initialization was finished
-// And system initialization was completed
-void ready() {
-	debugf("%9d: Ready, connect", RTC.getRtcSeconds());
-  //
-  WifiStation.waitConnection(connectOk, 5, connectFail);
-}
+  // Will be called when WiFi station timeout was reached
+  void onConnectFail()
+  {
+    failure_detected = true;
+    debugf("%9d: Connect failed!", RTC.getRtcSeconds());
+    reconnectAfterMs(2000);
+    digitalWrite(RELAY_PIN, false);
+  }
+  
+public:
+  bool failure_detected;
+};
+
 
 void init() {
 	Serial.begin(SERIAL_BAUD_RATE);
 	Serial.systemDebugOutput(true); // Allow debug print to serial
 	Serial.println("Thanx, Sming!");
-  
-  digitalWrite(RELAY_PIN, false);
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(LED_PIN, true);
-  pinMode(LED_PIN, OUTPUT);
 
   //SET higher CPU freq & disable wifi sleep
   system_update_cpu_freq(SYS_CPU_160MHZ);
   wifi_set_sleep_type(NONE_SLEEP_T);
-
-	// Set system ready callback method
-	System.onReady(ready);
-
-  // Station - WiFi client
-  WifiStation.enable(true);
-  WifiStation.config(WIFI_SSID, WIFI_PWD); // Put you SSID and Password here
-
-  // Optional: Change IP addresses (and disable DHCP)
-  WifiStation.setIP(WIFI_STATIC_IP);
-
-  // Print available access points
-  WifiStation.startScan(listNetworks); // In Sming we can start network scan from init method without additional code
-
+  
+  // realy and led init
+  digitalWrite(LED_PIN, true);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, false);
+  pinMode(RELAY_PIN, OUTPUT);
+  
+  //
+  WifiRouterChecker checker;
 }
